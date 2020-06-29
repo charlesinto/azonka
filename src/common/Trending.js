f3361b8742091187d80813fe3c48e7fe6bb4c2d7
nonsodaniel <nonsodaniel07@gmail.com>
2020-06-29 14:58:33 +0200

    Refactored UI

--- src/common/Trending.js
+++ src/common/Trending.js
@@ -5,7 +5,7 @@ class Trending extends Component {
     render() {
         return (
             <div className="col-lg-3  col-sm-3" key={this.props.index}>
-                <div className="banner shadow banner-image border">
+                <div className="banner  banner-image ">
                     <Link to="#featured">
                         <img src={this.props.image} className="d-block w-100" alt="banner" />
                     </Link>
